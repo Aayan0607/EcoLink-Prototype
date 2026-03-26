3e926ffe08b270b213a281897ab232690a812f25
Aayan Tamboli <tamboliaayan4225@gmail.com>
2026-03-26 16:42:18 +0530

    made config public

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA0WPUUPTuWSJqWsx_gT2GIT6oMRHvxn8w",
+    authDomain: "eco-link-prototype.firebaseapp.com",
+    projectId: "eco-link-prototype",
+    storageBucket: "eco-link-prototype.firebasestorage.app",
+    messagingSenderId: "1032200794760",
+    appId: "1:1032200794760:web:7d4730299a945689f1653d"
+  };
